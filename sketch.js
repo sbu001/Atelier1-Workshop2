06a18dd78a23323d54c202b1e8be2170742c455e
sbu010 <seungbiyoo433@gmail.com>
2025-10-13 16:42:19 -0400

    Back to the inital

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 let cat;
+let orientationX = 0;
+let orientationY = 0;
+let orientationZ = 0;
+
 
 function preload()
 {
@@ -7,33 +11,60 @@ function preload()
 
 function setup() 
 {
-	createCanvas(windowWidth, windowHeight);
-    showDebug();
-    enableGyroTap();
+	createCanvas(windowWidth, window);
+    //showDebug();
+    enableGyroTap('ENABLE MOTION SENSORS', 'Requesting motion sensors...');
     lockGestures();
     angleMode(DEGREES);
-    debug("Orientation Basic - Minimal Version");
-    debug("Tilt your device to see orientation values");
-    debug("Waiting for sensor data...");
+
+    textAlign(CENTER, CENTER);
+    textSize();
 }
 
 function draw() 
 {
-    // background(0, 0, 0, 25);
-	// image(cat, rx, ry, rx, 200, 240);
+    background(240, 240, 240, 20);
 
     if(window.sensorsEnabled){
-        let rx = rotationX;
-        let ry = rotationY;
-        let rz = rotationZ;
-        debug("--- Device Orientation ---");
-        debug("Rotation X (Tilt Forward/Back): " + int(rx) + "°");
-        debug("Rotation Y (Tilt Left/Right): " + int(ry) + "°");
-        debug("Rotation Z (Turn/Compass): " + int(rz) + "°");
 
-        //image(cat, rx, ry, rx, 200, 240);
+        background(0, 0, 0, 10);
+        orientationX = rotationX;
+        orientationY = rotationY;
+        orientationZ = rotationZ;
+
+        let catX = map(orientationY, -90, 90, 0, width);
+        let catY = map(orientationX, -90, 90, 0, height);
+        imageMode(CENTER);
+        push();
+        translate(catX, catY);
+        rotate(orientationZ);
+        image(cat, 0, 0, 300, 300);
+        pop();
+
+        textSize(14);
+        fill(100, 100, 100);
+        text("Tilt your device to move the GIF!", width/2, height - 30);
+
     } else{
-        debug("Sensors not enabled");
-        debug("Tap the screen to enable sensors");
+        fill(50, 50, 50);
+        textAlign(CENTER, CENTER);
+        textSize(28);
+        fill(150, 50, 50);
+        text("Motion Sensors Disabled", width/2, height/2 - 40);
+        
+        textSize(20);
+        fill(100, 100, 100);
+        text("Tap the screen to enable\nmotion sensors", width/2, height/2 + 20);
     }
+}
+
+function touchStarted() 
+{
+    // Permission handling is done by enableGyroTap
+    return false;
+}
+
+function touchEnded() 
+{
+    return false;
 }
