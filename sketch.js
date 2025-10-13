7565374e951706be844abb3f96707b3493c6c006
sbu010 <seungbiyoo433@gmail.com>
2025-10-13 16:43:15 -0400

    Back to the initial

--- sketch.js
+++ sketch.js
@@ -1,8 +1,4 @@
 let cat;
-let orientationX = 0;
-let orientationY = 0;
-let orientationZ = 0;
-
 
 function preload()
 {
@@ -11,60 +7,14 @@ function preload()
 
 function setup() 
 {
-	createCanvas(windowWidth, window);
-    //showDebug();
-    enableGyroTap('ENABLE MOTION SENSORS', 'Requesting motion sensors...');
-    lockGestures();
-    angleMode(DEGREES);
-
-    textAlign(CENTER, CENTER);
-    textSize();
+	createCanvas(windowWidth, windowHeight);
 }
 
-function draw() 
+function draw()
 {
-    background(240, 240, 240, 20);
+background(20, 200, 20);
+image(cat, width/2, height/2, 100, 120);
 
-    if(window.sensorsEnabled){
-
-        background(0, 0, 0, 10);
-        orientationX = rotationX;
-        orientationY = rotationY;
-        orientationZ = rotationZ;
-
-        let catX = map(orientationY, -90, 90, 0, width);
-        let catY = map(orientationX, -90, 90, 0, height);
-        imageMode(CENTER);
-        push();
-        translate(catX, catY);
-        rotate(orientationZ);
-        image(cat, 0, 0, 300, 300);
-        pop();
-
-        textSize(14);
-        fill(100, 100, 100);
-        text("Tilt your device to move the GIF!", width/2, height - 30);
-
-    } else{
-        fill(50, 50, 50);
-        textAlign(CENTER, CENTER);
-        textSize(28);
-        fill(150, 50, 50);
-        text("Motion Sensors Disabled", width/2, height/2 - 40);
-        
-        textSize(20);
-        fill(100, 100, 100);
-        text("Tap the screen to enable\nmotion sensors", width/2, height/2 + 20);
-    }
-}
-
-function touchStarted() 
-{
-    // Permission handling is done by enableGyroTap
-    return false;
-}
-
-function touchEnded() 
-{
-    return false;
+fill(255)
+ellipse(100, 100, 100, 100);
 }
